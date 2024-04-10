97011da4c3f53ebe98a16ae466abdff8a23c0298
Tuan Le <leanhtuan030523@gmail.com>
2024-04-10 14:54:51 +0700

    Create Vendor serializer

---
+++ ecommerce/vendors/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Vendor
+from users.serializers import UserSerializer
+from users.models import CustomUser
+
+class VendorSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    
+    class Meta: 
+        model = Vendor
+        fields = ("user", "address", "city", "phone_number", "shop_name", "description")
+        
+    def create(self, validated_data) -> Vendor:
+        user_data = validated_data.pop("user")
+        user = CustomUser.objects.create_user(**user_data)
+        
+        vendor = Vendor.objects.create(user=user, **validated_data)
+        return vendor
