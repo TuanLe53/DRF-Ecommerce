9b927183187631da63017b3d804695f17411032e
Tuan Le <leanhtuan030523@gmail.com>
2024-04-10 14:49:56 +0700

    Create customer model

--- ecommerce/customers/models.py
+++ ecommerce/customers/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from users.models import CustomUser
+from phonenumber_field.modelfields import PhoneNumberField
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+class Customer(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
+    city = models.CharField(_("city"), blank=False, null=False, max_length=20)
+    address = models.CharField(_("address"), blank=False, null=False, max_length=250)
+    phone_number = PhoneNumberField()
+    avatar = models.ImageField(upload_to="customer_avatar", null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.user.email
