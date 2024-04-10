9ffa5cadd3794a199d153db402e28ff0f662acd7
Tuan Le <leanhtuan030523@gmail.com>
2024-04-10 16:09:37 +0700

    Add ListCreateCategory view

--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from .serializers import CategorySerializer
+from .models import Category
 # Create your views here.
+
+class ListCreateCategory(generics.ListCreateAPIView):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
