d84824e00dec167de19adeeadd534ace6c5a845e
Tuan Le <leanhtuan030523@gmail.com>
2024-04-10 14:55:32 +0700

    Create vendor registor view

--- ecommerce/vendors/views.py
+++ ecommerce/vendors/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
+from .models import Vendor
+from .serializers import VendorSerializer
 # Create your views here.
+
+class CreateVendor(CreateAPIView):
+    permission_classes = (AllowAny, )
+    queryset = Vendor.objects.all()
+    serializer_class = VendorSerializer
