564bd51d283a63a786ab0d04677dfba2dfabcb22
Tuan Le <leanhtuan030523@gmail.com>
2024-04-10 14:00:56 +0700

    Create custom user model

--- ecommerce/users/models.py
+++ ecommerce/users/models.py
@@ -1,3 +1,30 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from .managers import CustomManager
 
 # Create your models here.
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    USER_TYPE_CHOICES = (
+        ("VENDOR", "Vendor"),
+        ("CUSTOMER", "Customer"),
+    )
+    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    email = models.EmailField(_("email_address"), unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(_("first name"), max_length=150, blank=False)
+    last_name = models.CharField(_("last name"), max_length=150, blank=False)
+    user_type = models.CharField(choices=USER_TYPE_CHOICES, max_length=15)   
+     
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    
+    objects = CustomManager()
+    
+    def __str__(self):
+        return self.username
