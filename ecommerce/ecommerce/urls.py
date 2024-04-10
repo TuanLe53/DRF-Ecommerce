e5b351d5eb5dfb7920cd9561ad08a9ff4cbf5d7e
Tuan Le <leanhtuan030523@gmail.com>
2024-04-10 14:49:35 +0700

    Add customers urls

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -22,6 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("users.urls")),
-    # path("customer/", include("customers.urls")),
+    path("customer/", include("customers.urls")),
     # path("vendor/", include("vendors.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
