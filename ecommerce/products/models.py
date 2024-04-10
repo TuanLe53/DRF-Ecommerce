4f93a6502a0308b0c8e300b155b6b14940dd62d5
Tuan Le <leanhtuan030523@gmail.com>
2024-04-10 15:00:26 +0700

    Create products app

---
+++ ecommerce/products/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+import uuid
+from vendors.models import Vendor
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    slug = models.SlugField(max_length=200, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+    
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4, unique=True)
+    name = models.CharField(max_length=200,null=True)
+    slug = models.SlugField(max_length=200,db_index=True)
+    price = models.FloatField()
+    description = models.TextField(blank=True, null=True)
+    
+    category = models.ManyToManyField(Category, related_name='categories')
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, related_name='products')
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.name
+    
+class ProductImages(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="images")
+    image = models.ImageField(upload_to="product")
+    
+    def __str__(self):
+        return f"{self.image.url}"
+    
+class Inventory(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.product}: {self.quantity}"
