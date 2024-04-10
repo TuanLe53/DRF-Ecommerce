468cf56ec7cd1b8d5a0997053bc5d96381ba3781
Tuan Le <leanhtuan030523@gmail.com>
2024-04-11 05:32:53 +0700

    Add ListCreateProduct url

--- ecommerce/products/urls.py
+++ ecommerce/products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ListCreateCategory
+from .views import ListCreateCategory, ListCreateProduct
 
 urlpatterns = [
+    path("", ListCreateProduct.as_view(), name="products"),
     path("category/", ListCreateCategory.as_view(), name="category"),
 ]
