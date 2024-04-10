c5bc078f83cc802120578157b913b5a18011e12d
Tuan Le <leanhtuan030523@gmail.com>
2024-04-10 14:50:27 +0700

    Create Register customer view

--- ecommerce/customers/views.py
+++ ecommerce/customers/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
+from .models import Customer
+from .serializers import CustomerSerializer
 
 # Create your views here.
+class CreateCustomer(CreateAPIView):
+    permission_classes = (AllowAny, )
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
