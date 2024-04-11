9df916d5fb4e75d7c462346e4a62e19a0d5d2d04
Tuan Le <leanhtuan030523@gmail.com>
2024-04-11 09:44:05 +0700

    Add retrieve update delete view

--- ecommerce/products/urls.py
+++ ecommerce/products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ListCreateCategory, ListCreateProduct
+from .views import ListCreateCategory, ListCreateProduct, RetrieveUpdateDeleteProductByID
 
 urlpatterns = [
     path("", ListCreateProduct.as_view(), name="products"),
+    path("<uuid:pk>", RetrieveUpdateDeleteProductByID.as_view(), name="product"),
     path("category/", ListCreateCategory.as_view(), name="category"),
 ]
