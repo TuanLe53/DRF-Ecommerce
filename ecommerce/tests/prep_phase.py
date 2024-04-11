ccd365a30be6b94ffc2bb3e533c6d9d7cb1325a6
Tuan Le <leanhtuan030523@gmail.com>
2024-04-11 09:42:29 +0700

    Add prep_pd

--- ecommerce/tests/prep_phase.py
+++ ecommerce/tests/prep_phase.py
@@ -1,5 +1,9 @@
 from django.urls import reverse
 from users.models import CustomUser
+from products.models import Category
+from django.core.files.uploadedfile import SimpleUploadedFile
+from PIL import Image
+from io import BytesIO
 
 register_payload = {
 	"user":{
@@ -24,3 +28,51 @@ def prep_user(client) -> None:
     login = client.post(reverse("login"), data=login_payload, format="json")
     access_token = login.data["access"]
     client.credentials(HTTP_AUTHORIZATION=f"Bearer {access_token}")
+    
+def prep_products(client, k:int):
+    Category.objects.create(
+        name="shirt",
+        description="This is a t-shirt category",
+        slug="shirt"
+        )
+    size = (400, 400)
+    storage = BytesIO()
+    img = Image.new("RGB", size)
+    img.save(storage, "JPEG")
+    storage.seek(0)
+    file = SimpleUploadedFile(
+        name="test_img.jpg",
+        content=storage.getvalue(),
+        content_type="image/jpeg"
+    )
+    
+    for i in range(k):
+        product = {
+            "name": f"test product {i}",
+            "sku": "WW2906AS",
+            "description": "description",
+            "price": 199000 + i,
+            "quantity": 10 + i,
+            "categories": "shirt",
+            "slug": "shirt",
+            "images": file,
+        }
+        client.post(reverse("products"), data = product, format="multipart")
+
+    Category.objects.create(
+        name="food",
+        description="this is a food category",
+        slug="food"
+        )
+    for _ in range(5):
+        product = {
+            "name": f"Food {_}",
+            "sku": "WW2906AS",
+            "description": "description",
+            "price": 199000 + i,
+            "quantity": 10 + i,
+            "categories": "food",
+            "slug": "food",
+            "images": file,
+        }
+        client.post(reverse("products"), data = product, format="multipart")
