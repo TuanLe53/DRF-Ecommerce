0735a4f627456435f2590b6e591f251941bd48b8
Tuan Le <leanhtuan030523@gmail.com>
2024-04-11 14:42:03 +0700

    Add CreateCartItem path

--- ecommerce/customers/urls.py
+++ ecommerce/customers/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CreateCustomer, ListCreateCart
+from .views import CreateCustomer, ListCreateCart, CreateCartItem
 
 urlpatterns = [
     path("new/", CreateCustomer.as_view(), name="customer_register"),
-    path("cart/", ListCreateCart.as_view(), name="cart")
+    path("cart/", ListCreateCart.as_view(), name="cart"),
+    path("cart/items/", CreateCartItem.as_view(), name="cart_item"),
 ]
