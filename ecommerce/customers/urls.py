68b5520e4735d0959062fa959bfbe114d1ed53ed
Tuan Le <leanhtuan030523@gmail.com>
2024-04-11 15:08:02 +0700

    Add cart_items path and remove_item path

--- ecommerce/customers/urls.py
+++ ecommerce/customers/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import CreateCustomer, ListCreateCart, CreateCartItem
+from .views import CreateCustomer, ListCreateCart, CreateCartItem, ListCartItem, DeleteCartItem
 
 urlpatterns = [
     path("new/", CreateCustomer.as_view(), name="customer_register"),
     path("cart/", ListCreateCart.as_view(), name="cart"),
-    path("cart/items/", CreateCartItem.as_view(), name="cart_item"),
+    path("cart/items/", CreateCartItem.as_view(), name="add_item"),
+    path("cart/<uuid:pk>", ListCartItem.as_view(), name="cart_items"),
+    path("cart/items/<int:pk>", DeleteCartItem.as_view(), name="remove_item"),
 ]
