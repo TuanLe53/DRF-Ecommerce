c04c8a751371e91c9089dea2faa4d3b641e77e11
Tuan Le <leanhtuan030523@gmail.com>
2024-04-11 09:43:35 +0700

    Create IsProductOwner permission class

--- ecommerce/products/permissions.py
+++ ecommerce/products/permissions.py
@@ -5,3 +5,15 @@ class IsVendor(BasePermission):
         if request.user.is_authenticated and request.user.user_type == "VENDOR":
             return True
         return False
+    
+class IsProductOwner(BasePermission):
+    accept_methods = ("PUT", "DELETE", "GET")
+    def has_permission(self, request, view):
+        if request.user.is_authenticated and request.user.user_type == "VENDOR":
+            return True
+        return False
+    
+    def has_object_permission(self, request, view, obj):
+        if obj.vendor.user == request.user and request.method in self.accept_methods:
+            return True
+        return False
