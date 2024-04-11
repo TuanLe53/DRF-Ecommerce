b8fa70e331d344a02c321f496b767fde250e1e52
Tuan Le <leanhtuan030523@gmail.com>
2024-04-11 19:59:46 +0700

    Create OrderSerializer

---
+++ ecommerce/orders/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Order
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = "__all__"
+        extra_kwargs = {
+            "address": {"read_only": True}
+        }
