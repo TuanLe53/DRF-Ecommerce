328eea494c3293717f5ada597dcddc2023614904
Tuan Le <leanhtuan030523@gmail.com>
2024-04-11 18:04:58 +0700

    Create payment model

--- ecommerce/payments/models.py
+++ ecommerce/payments/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+import uuid
+from customers.models import Customer
 
 # Create your models here.
+class Payment(models.Model):
+    PROVIDER_CHOICES={
+        ("VISA", "Visa"),
+        ("MASTERCARD", "Mastercard"),
+    }
+    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="payments")
+    provider = models.CharField(choices=PROVIDER_CHOICES, max_length=12)
+    account_number = models.CharField()
+    expiry_date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_default = models.BooleanField(default=False)
