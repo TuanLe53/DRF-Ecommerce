c8d9a4728d42e9a33655c69f5199def522459825
Tuan Le <leanhtuan030523@gmail.com>
2024-04-11 14:40:47 +0700

    Add __str__ method to Cart and CartItem

--- ecommerce/customers/models.py
+++ ecommerce/customers/models.py
@@ -28,9 +28,15 @@ class Cart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
+    def __str__(self) -> str:
+        return f"CartID: {self.id}"
+    
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self) -> str:
+        return f"{self.product.name}: {self.quantity}"
