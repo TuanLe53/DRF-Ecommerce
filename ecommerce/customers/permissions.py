4938a2e332f5a458eb575b301f3b52dbe8d04707
Tuan Le <leanhtuan030523@gmail.com>
2024-04-11 15:07:33 +0700

    Create IsCartOwner permission class

--- ecommerce/customers/permissions.py
+++ ecommerce/customers/permissions.py
@@ -5,3 +5,14 @@ class IsCustomer(BasePermission):
         if request.user.is_authenticated and request.user.user_type == "CUSTOMER":
             return True
         return False
+    
+class IsCartOwner(BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated and request.user.user_type == "CUSTOMER":
+            return True
+        return False
+    
+    def has_object_permission(self, request, view, obj):
+        if obj.cart.customer.user == request.user:
+            return True
+        return False
