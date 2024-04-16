5c87a8e0cb1d4b3e228f7460bdabec3681c0f65e
Tuan Le <leanhtuan030523@gmail.com>
2024-04-16 15:53:52 +0700

    Create Router, ChakraProvider, QueryClientProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { ChakraProvider } from "@chakra-ui/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import Home from "./pages/home.jsx";
+import Login from "./pages/login.jsx";
+import Register from "./pages/register.jsx";
+
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/register",
+    element: <Register />
+  }
+])
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router}/>
+      </QueryClientProvider>
+    </ChakraProvider>
   </React.StrictMode>,
 )
