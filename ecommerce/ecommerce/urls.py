163577d33c7903b1f3f97ef4da4dd69067abcbf4
Tuan Le <leanhtuan030523@gmail.com>
2024-04-18 14:50:24 +0700

    Change users routes from auth/ to user/

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("auth/", include("users.urls")),
+    path("user/", include("users.urls")),
     path("customer/", include("customers.urls")),
     path("vendor/", include("vendors.urls")),
     path("product/", include("products.urls")),
