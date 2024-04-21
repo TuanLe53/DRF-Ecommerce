652569a3df6eb79b9733bb612bf8f1982f973f6e
Tuan Le <leanhtuan030523@gmail.com>
2024-04-21 18:55:43 +0700

    Adding products by vendor endpoint

--- ecommerce/products/urls.py
+++ ecommerce/products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ListCreateCategory, ListCreateProduct, RetrieveUpdateDeleteProductByID
+from .views import ListCreateCategory, ListCreateProduct, RetrieveUpdateDeleteProductByID, ListProductsByID
 
 urlpatterns = [
     path("", ListCreateProduct.as_view(), name="products"),
     path("<uuid:pk>", RetrieveUpdateDeleteProductByID.as_view(), name="product"),
     path("category/", ListCreateCategory.as_view(), name="category"),
+    path("vendor/", ListProductsByID.as_view(), name="product_by_vendor")
 ]
