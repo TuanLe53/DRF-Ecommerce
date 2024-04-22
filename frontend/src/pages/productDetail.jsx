5b4f060ef43532c9bf85767b9fcaa974327ffea7
Tuan Le <leanhtuan030523@gmail.com>
2024-04-22 11:12:06 +0700

    Create productDetail page

---
+++ frontend/src/pages/productDetail.jsx
@@ -0,0 +1,10 @@
+import { Text } from "@chakra-ui/react";
+
+export default function ProductDetail() {
+    
+    return (
+        <>
+            <Text>This page should includes product's information </Text>
+        </>
+    )
+}
