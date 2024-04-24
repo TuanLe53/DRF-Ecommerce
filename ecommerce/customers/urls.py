b3a6d6d0eb453bc5d37798ffebb0c527070a27d5
Tuan Le <leanhtuan030523@gmail.com>
2024-04-24 10:45:22 +0700

    Combine CreateCartItem and ListCartItem into a single view.

--- ecommerce/customers/urls.py
+++ ecommerce/customers/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from .views import CreateCustomer, ListCreateCart, CreateCartItem, ListCartItem, DeleteCartItem
+from .views import CreateCustomer, DeleteCartItem, ListCreateCartItem
 
 urlpatterns = [
     path("new/", CreateCustomer.as_view(), name="customer_register"),
-    path("cart/", ListCreateCart.as_view(), name="cart"),
-    path("cart/items/", CreateCartItem.as_view(), name="add_item"),
-    path("cart/<uuid:pk>", ListCartItem.as_view(), name="cart_items"),
+    path("cart/", ListCreateCartItem.as_view(), name="cart_items"),
     path("cart/items/<int:pk>", DeleteCartItem.as_view(), name="remove_item"),
 ]
--- ecommerce/customers/views.py
+++ ecommerce/customers/views.py
@@ -1,11 +1,11 @@
-from rest_framework.generics import CreateAPIView, ListCreateAPIView, ListAPIView, DestroyAPIView
+from rest_framework.generics import CreateAPIView, ListCreateAPIView, DestroyAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 from django.shortcuts import get_object_or_404
 
 from .models import Customer, Cart, CartItem
-from .serializers import CustomerSerializer, CartSerializer, CartItemSerializer
+from .serializers import CustomerSerializer, CartItemSerializer
 from .permissions import IsCustomer, IsCartOwner
 
 # Create your views here.
@@ -14,41 +14,27 @@ class CreateCustomer(CreateAPIView):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
     
-# class ListCreateCart(ListCreateAPIView):
-#     queryset = Cart.objects.all()
-#     serializer_class = CartSerializer
-#     permission_classes = (IsCustomer, )
-    
-#     def post(self, request):
-#         customer = Customer.objects.get(user=request.user)
-#         cart = Cart.objects.create(
-#             customer = customer,
-#             status = "OPEN"
-#         )
-        
-#         serializer = CartSerializer(cart)
-        
-#         return Response(serializer.data, status=201)
-    
-class CreateCartItem(CreateAPIView):
+class ListCreateCartItem(ListCreateAPIView):
     queryset = CartItem.objects.all()
     serializer_class = CartItemSerializer
-    permission_classes = (IsCustomer, )
+    
+    def get_permissions(self):
+        if self.request.method == "GET":
+            self.permission_classes = (IsCartOwner, )
+        else:
+            self.permission_classes = (IsCustomer, )
+        return super(ListCreateCartItem, self).get_permissions()
+    
+    def get_queryset(self):
+        customer = get_object_or_404(Customer, user=self.request.user)
+        cart = get_object_or_404(Cart, customer=customer)
+        return CartItem.objects.filter(cart=cart)
     
     def post(self, request):
         serializer = CartItemSerializer(data=request.data, context={"request": request})
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=201)
-    
-class ListCartItem(ListAPIView):
-    queryset = CartItem.objects.all()
-    serializer_class = CartItemSerializer
-    permission_classes = (IsCartOwner, )
-    
-    def get_queryset(self):
-        cart = get_object_or_404(Cart, id=self.kwargs["pk"])
-        return CartItem.objects.filter(cart=cart)
     
 class DeleteCartItem(DestroyAPIView):
     queryset = CartItem.objects.all()
