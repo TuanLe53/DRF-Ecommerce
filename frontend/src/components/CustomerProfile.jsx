3237164f3b79ff7b4c9a9dcb6eab89a5d95b72e9
Tuan Le <leanhtuan030523@gmail.com>
2024-05-08 19:10:48 +0700

    Create OrderTab and fetch orders

--- frontend/src/components/CustomerProfile.jsx
+++ frontend/src/components/CustomerProfile.jsx
@@ -1,5 +1,5 @@
 import { ShoppingCartOutlined, ShoppingOutlined } from "@ant-design/icons";
-import { AddIcon, DeleteIcon, Icon } from "@chakra-ui/icons";
+import { AddIcon, Icon } from "@chakra-ui/icons";
 import {
     Tabs,
     TabList,
@@ -7,14 +7,9 @@ import {
     TabPanels,
     TabPanel,
     Card,
-    Image,
-    Stack,
     Select,
     Text,
-    CardFooter,
     IconButton,
-    Flex,
-    Spacer,
     Box,
     useDisclosure,
     Button,
@@ -37,10 +32,12 @@ import {
     Heading
 } from "@chakra-ui/react";
 import { useMutation, useQuery } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
 import { useContext, useState } from "react";
 import AuthContext from "../contexts/AuthContext";
 import {VNDDong} from "../utils/VNDDongFormat"
 import { useBoundStore } from "../store/store";
+import CartItem from "./CartItem";
 
 export default function CustomerProfile() {
 
@@ -58,7 +55,9 @@ export default function CustomerProfile() {
                 <TabPanel>
                     <PaymentTab />
                 </TabPanel>
-                <TabPanel><p>Orders</p></TabPanel>
+                <TabPanel>
+                    <OrderTab />
+                </TabPanel>
             </TabPanels>
         </Tabs>
     )
@@ -67,60 +66,10 @@ export default function CustomerProfile() {
 function CartTab() {
     const { accessToken } = useContext(AuthContext);
 
+    const navigate = useNavigate();
+
     const cartItems = useBoundStore((state) => state.cartItems);
     const setCartItems = useBoundStore((store) => store.setCartItems);
-    const removeItem = useBoundStore((store) => store.removeItem);
-
-    const [targetItem, setTargetItem] = useState({
-        name: "",
-        id: ""
-    });
-
-    const toast = useToast();
-
-    const {isOpen, onOpen, onClose} = useDisclosure();
-
-    const openConfirmModal = (productName, itemID) => {
-        setTargetItem({
-            name: productName,
-            id: itemID
-        })
-        onOpen()
-    }
-
-    const {isPending:RemovePending, mutate:RemoveItem} = useMutation({
-        mutationFn: async () => {
-            const res = await fetch(`http://127.0.0.1:8000/customer/cart/item/${targetItem.id}`, {
-                method: "DELETE",
-                headers: {"Authorization": `Bearer ${String(accessToken)}`,}
-            })
-
-            if (res.status !== 204) throw new Error("Failed to delete item from cart.");;
-
-            return "Success"
-        },
-        onSuccess: () => {
-            removeItem(targetItem.id)
-            toast({
-                title: 'Success',
-                description: "Removed from cart",
-                status: 'success',
-                duration: 5000,
-                isClosable: true,
-            })
-            onClose()
-        },
-        onError: (error) => {
-            toast({
-                title: 'Error',
-                description: "Something went wrong. Please try again later.",
-                status: 'error',
-                duration: 5000,
-                isClosable: true,
-            })
-            onClose()
-        }
-    })
 
     const fetchCartItems = async () => {
         let res = await fetch("http://127.0.0.1:8000/customer/cart/", {
@@ -153,53 +102,10 @@ function CartTab() {
     return (
         <>
             {cartItems.map((item, i) => (
-                <Card key={i} direction={"row"} marginBottom={"5px"} p={"10px"}>
-                    <Image
-                        objectFit={"contain"}
-                        src={item.product.images[0].image}
-                        maxW={"100px"}
-                    />
-                    <Flex w="100%" align={"center"}>
-                        <Box>
-                            <Text>{item.product.name}</Text>
-                            <Text>Quantity: {item.quantity}</Text>
-                        </Box>
-                        <Spacer />
-                        <Stack>
-                            <Box textAlign={"right"}>
-                                <Text>{VNDDong.format(item.product.price)}</Text>
-                                <Text>Total: {VNDDong.format(item.product.price * item.quantity)}</Text>
-                            </Box>
-                        </Stack>
-                        
-                        <CardFooter>
-                            <IconButton
-                                aria-label="Remove item"
-                                icon={<DeleteIcon color={"red"}/>}
-                                variant={"outline"}
-                                float={"right"}
-                                onClick={() => openConfirmModal(item.product.name, item.id)}
-                                />
-                        </CardFooter>
-                    </Flex>
-                </Card>
+                <CartItem key={i} item={item}/>
             ))}
             <Text textAlign={"right"}>Total: {VNDDong.format(totalEstimate())}</Text>
-            <Button float={"right"}>Order</Button>
-            <Modal isOpen={isOpen} onClose={onClose}>
-                <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>Remove Item</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody>
-                        <Text>Are you sure you want to remove {targetItem.name} from your cart?</Text>
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button colorScheme="blue" onClick={onClose}>No</Button>
-                        <Button isLoading={RemovePending} onClick={RemoveItem}>Yes</Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
+            <Button isDisabled={cartItems.length === 0} onClick={() => navigate("/orders/new")} float={"right"}>Place Order</Button>
         </>
     )
 }
@@ -279,7 +185,6 @@ function PaymentTab() {
         queryFn: fetchPayments
     })
     
-    if (payments) console.log(payments);
     if (isPending) return "Loading";
     if (error) return "An error has occurred";
 
@@ -345,6 +250,34 @@ function PaymentTab() {
                     </form>
                 </ModalContent>
             </Modal>
+        </>
+    )
+}
+
+function OrderTab() {
+    const { accessToken } = useContext(AuthContext);
+
+    const fetchOrders = async () => {
+        const res = await fetch("http://127.0.0.1:8000/order/", {
+            headers: {"Authorization": `Bearer ${String(accessToken)}`,},
+        })
+        const data = await res.json()
+        if (res.status !== 200) throw data;
+
+        return data
+    }
+
+    const { isPending, error, data} = useQuery({
+        queryKey: ["orders"],
+        queryFn:fetchOrders
+    })
+
+    if (data) console.log(data);
+    if (error) console.log(error);
+
+    return (
+        <>
+            <p>Pyament Hehehehe</p>
         </>
     )
 }
