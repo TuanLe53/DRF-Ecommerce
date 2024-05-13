464913ca8ed0521a4d2a4a71f108c5572e7404f1
Tuan Le <leanhtuan030523@gmail.com>
2024-05-13 18:22:53 +0700

    Create routes folder

--- frontend/src/components/routes/ProtectedRoute.jsx
+++ frontend/src/components/routes/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import AuthContext from "../contexts/AuthContext";
+import AuthContext from "../../contexts/AuthContext";
 
 export default function ProtectedRoute({ children }) {
     const { user } = useContext(AuthContext);
