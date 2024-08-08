19ca8545595e7294b04468b035c76f6863e7a909
Tuan Le <leanhtuan030523@gmail.com>
2024-08-08 16:38:31 +0700

    Delete old Frontend

--- frontend/src/store/cartSlice.js
+++
@@ -1,7 +0,0 @@
-export const createCartSlice = (set) => ({
-    cartItems: [],
-    setCartItems: (cartItems) => set({ cartItems }),
-    removeItem: (item_id) => set(state => ({
-        cartItems: state.cartItems.filter(item => item.id !== item_id)
-    }))
-})
--- frontend/src/store/store.js
+++
@@ -1,6 +0,0 @@
-import { create } from "zustand";
-import { createCartSlice } from "./cartSlice";
-
-export const useBoundStore = create((...a) => ({
-    ...createCartSlice(...a),
-}));
