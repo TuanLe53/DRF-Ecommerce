5aac02e4eee8aabd9c52290e43a175c2abe8adc3
Tuan Le <leanhtuan030523@gmail.com>
2024-08-08 19:22:03 +0700

    Install Shadcn/UI

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import viteReact from '@vitejs/plugin-react'
-import {TanStackRouterVite} from '@tanstack/router-plugin/vite'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,4 +9,9 @@ export default defineConfig({
     TanStackRouterVite(),
     viteReact(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  }
 })
