83602ec4077e507b3b82d3f50261bb26d2c6c998
Tuan Le <leanhtuan030523@gmail.com>
2024-08-09 08:56:38 +0700

    Install and setup Tanstack router

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,13 @@
 import path from "path"
-import react from "@vitejs/plugin-react"
+import viteReact from '@vitejs/plugin-react'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import { defineConfig } from "vite"
  
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    viteReact(),
+    TanStackRouterVite()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
