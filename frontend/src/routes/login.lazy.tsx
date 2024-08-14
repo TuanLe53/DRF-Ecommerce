629f4dc9e760873249cf51161f09b55791cb1760
Tuan Le <leanhtuan030523@gmail.com>
2024-08-14 09:41:14 +0700

    Style header and create loginFormSchema

--- frontend/src/routes/login.lazy.tsx
+++ frontend/src/routes/login.lazy.tsx
@@ -1,13 +1,20 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
+import { z } from 'zod'
+
+const loginFormSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6,  { message: "Password is too short" }).max(50, { message: "Password is too long" }),
+})
 
 export const Route = createLazyFileRoute('/login')({
   component: Login
 })
 
 function Login() {
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className='flex flex-col items-center'>
+      <h1 className='font-semibold text-3xl mb-3'>Login</h1>  
       <form>
         
       </form>
