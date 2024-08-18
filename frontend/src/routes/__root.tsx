aee0d500e91ac4a1e860419d108d8e5d770b4463
Tuan Le <leanhtuan030523@gmail.com>
2024-08-18 19:24:33 +0700

    Update

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,6 +1,6 @@
-import { ContextProps } from '@/contexts/authContext';
+import { ContextProps, useAuth } from '@/contexts/authContext';
 import { QueryClient } from '@tanstack/react-query';
-import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
+import { createRootRouteWithContext, Link, Outlet, redirect } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
 export interface RouterContext {
@@ -13,8 +13,29 @@ export const Route = createRootRouteWithContext<RouterContext>()({
 })
 
 function RootComponent() {
+  const { authState, logout } = useAuth();
+
+  const handleLogout = () => {
+    logout()
+    redirect({
+      to: '/login',
+    })
+  }
+
   return (
     <>
+      <div className='flex bg-red-200'>
+        <Link to='/' className='[&.active]:font-bold'>Home</Link>{' '}
+        <Link to='/about' className='[&.active]:font-bold'>About</Link>{' '}
+        {authState.isAuth ?
+          <div className='flex'>
+            <Link to='/profile'>Profile</Link>
+            <p onClick={handleLogout} className='hover:cursor-pointer'>Log out</p>
+          </div>
+          :
+          <Link to='/login'>Login</Link>
+        }
+      </div>
       <Outlet />
       <TanStackRouterDevtools />
     </>
