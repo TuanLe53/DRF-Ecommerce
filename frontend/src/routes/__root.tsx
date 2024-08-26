285240b33d08ed9d004f1f2826dfe41136ad2074
Tuan Le <leanhtuan030523@gmail.com>
2024-08-26 18:35:42 +0700

    Update styles

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -25,13 +25,20 @@ function RootComponent() {
 
   return (
     <>
-      <div className='flex bg-red-200'>
-        <Link to='/' className='[&.active]:font-bold'>Home</Link>{' '}
-        <Link to='/about' className='[&.active]:font-bold'>About</Link>{' '}
+      <div className='h-12 p-1 flex bg-red-200 items-center justify-between'>
+        <div className='flex gap-2'>
+          <Link to='/' className='text-lg [&.active]:font-bold'>Home</Link>
+          <Link to='/about' className='text-lg [&.active]:font-bold'>About</Link>
+        </div>
         {authState.isAuth ?
           <div className='flex'>
-            <Link to='/profile'>Profile</Link>
-            <p onClick={handleLogout} className='hover:cursor-pointer'>Log out</p>
+            <Link
+              to='/profile'
+              className='pr-1 mr-1 border-r-2 border-black hover:underline [&.active]:font-bold'
+            >
+              Profile
+            </Link>
+            <p onClick={handleLogout} className='hover:cursor-pointer hover:underline'>Log out</p>
           </div>
           :
           <Link to='/login'>Login</Link>
