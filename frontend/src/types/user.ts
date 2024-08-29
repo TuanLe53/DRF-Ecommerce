0dcc93d073f64531e9d4da25bc9fd7138871075b
Tuan Le <leanhtuan030523@gmail.com>
2024-08-29 15:46:27 +0700

    Add Payment interface

--- frontend/src/types/user.ts
+++ frontend/src/types/user.ts
@@ -3,3 +3,12 @@ export interface User{
     id: string | null;
     user_type: 'VENDOR' | 'CUSTOMER' | null;
 }
+
+export interface Payment{
+    id: string;
+    provider: string;
+    account_number: string;
+    expiry_date: string;
+    created_at: string;
+    is_default: boolean
+}
