6e8df25791821b5133a87adfc1e9a13f631a8b7b
Tuan Le <leanhtuan030523@gmail.com>
2024-09-01 20:01:05 +0700

    Create order.ts file

---
+++ frontend/src/types/order.ts
@@ -0,0 +1,27 @@
+import { ProductBasicInfo } from "./product";
+
+export interface Order {
+    id: string;
+    total_price: number;
+    address: string;
+    payment_type: "COD" | "CREDIT_CARD";
+    payment: string;
+    status: "PROCESSING" | "DELIVERING" | "RECEIVED";
+    created_at: string;
+    updated_at: string;
+    items: OrderItemBasicInfo[];
+}
+
+export type OrderBasicInfo = Pick<Order, "id" | "status" | "created_at" | "payment_type" | "total_price">
+
+export interface OrderItem{
+    id: string;
+    order: string;
+    quantity: number;
+    total_price: number;
+    created_at: string;
+    order_status: "PROCESSING" | "DELIVERING" | "RECEIVED";
+    product: ProductBasicInfo;
+}
+
+export type OrderItemBasicInfo = Pick<OrderItem, "quantity" | "total_price" | "product">;
