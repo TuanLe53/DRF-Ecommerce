8a62eb000cf8865e7631888dbf7c7b75dc4ec879
Tuan Le <leanhtuan030523@gmail.com>
2024-09-03 15:09:27 +0700

    Add CancelOrder to urlpatterns

--- ecommerce/orders/urls.py
+++ ecommerce/orders/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ListCreateOrder, ListOrderItem, RetrieveOrder
+from .views import ListCreateOrder, ListOrderItem, RetrieveOrder, CancelOrder
 
 urlpatterns = [
     path("", ListCreateOrder.as_view(), name="orders"),
     path("<str:pk>", RetrieveOrder.as_view(), name="order"),
+    path("<str:pk>/cancel/", CancelOrder.as_view(), name="cancel_order"),
     path("items/", ListOrderItem.as_view(), name="order_items")
 ]
