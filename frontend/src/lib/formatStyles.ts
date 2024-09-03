a97ca59bddd2297d61a137be36127d08aa6998cd
Tuan Le <leanhtuan030523@gmail.com>
2024-09-03 15:31:09 +0700

    Add red text to hanlde CANCELLED status

--- frontend/src/lib/formatStyles.ts
+++ frontend/src/lib/formatStyles.ts
@@ -2,6 +2,8 @@ export const setStatusColor = (status: string):string=>{
     return status === 'RECEIVED'
     ? 'text-green-500'
     : status ==='DELIVERING'
-        ? 'text-orange-500'
-        : '';
+            ? 'text-orange-500'
+            : status === 'CANCELLED'
+                ?'text-red-500'
+                : '';
 }
