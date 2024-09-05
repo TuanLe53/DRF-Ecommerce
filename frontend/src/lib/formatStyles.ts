db78ddf8890eb26cf31c25a631e412a4a8dda5a6
Tuan Le <leanhtuan030523@gmail.com>
2024-09-05 15:02:57 +0700

    Add toTitleCase function

--- frontend/src/lib/formatStyles.ts
+++ frontend/src/lib/formatStyles.ts
@@ -1,9 +1,15 @@
-export const setStatusColor = (status: string):string=>{
+export const setStatusColor = (status: string): string => {
     return status === 'RECEIVED'
-    ? 'text-green-500'
-    : status ==='DELIVERING'
+        ? 'text-green-500'
+        : status === 'DELIVERING'
             ? 'text-orange-500'
             : status === 'CANCELLED'
-                ?'text-red-500'
+                ? 'text-red-500'
                 : '';
-}
+};
+
+export function toTitleCase(str: string): string {
+  return str.replace(/\w\S*/g, (txt) => {
+    return txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase();
+  });
+};
