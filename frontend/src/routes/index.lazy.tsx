d4a9f0f2cbc44d56a4b38180f864820c164564a7
Tuan Le <leanhtuan030523@gmail.com>
2024-09-05 10:20:12 +0700

    Create ProductsByCategoryCarousel component

--- frontend/src/routes/index.lazy.tsx
+++ frontend/src/routes/index.lazy.tsx
@@ -1,3 +1,6 @@
+import ProductCard from '@/components/productCard';
+import { Product } from '@/types/product';
+import { useQuery } from '@tanstack/react-query';
 import { createLazyFileRoute } from '@tanstack/react-router'
 
 export const Route = createLazyFileRoute('/')({
@@ -8,6 +11,37 @@ function Index() {
   return (
     <div className="p-2">
       <h3>Welcome Home!</h3>
+      <ProductsByCategoryCarousel category='shirts'/>
+      <ProductsByCategoryCarousel category='sneaker'/>
+    </div>
+  )
+}
+
+interface ProductsByCategoryCarouselProps{
+  category: string;
+}
+
+function ProductsByCategoryCarousel({category}:ProductsByCategoryCarouselProps) {
+  const fetchProducts = async ():Promise<Product[]> => {
+    const res = await fetch(`http://127.0.0.1:8000/products/?category=${category}`);
+    if(!res.ok) throw new Error('An error has occurred. Please try again later.')
+  
+    return await res.json();
+  }
+
+  const {isPending, isError, data: products} = useQuery({
+    queryKey: ['products_by_category', category],
+    queryFn: fetchProducts
+  })
+
+  if (isPending) return <div>Loading...</div>
+  if(isError) return <div>Error</div>
+
+  return (
+    <div>
+      {products.map((product, index) => (
+        <ProductCard product={product} key={index}/>
+      ))}
     </div>
   )
 }
