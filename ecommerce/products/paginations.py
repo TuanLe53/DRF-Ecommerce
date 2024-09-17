af210772027b0e17954e6016ae87282521a79f68
Tuan Le <leanhtuan030523@gmail.com>
2024-09-17 19:57:52 +0700

    Add page_size_query_param, remove page_size

--- ecommerce/products/paginations.py
+++ ecommerce/products/paginations.py
@@ -1,6 +1,5 @@
 from rest_framework.pagination import PageNumberPagination
 
 class ProductsPagination(PageNumberPagination):
-    # PAGE_SIZE = 1 only for testing purpose
-    page_size = 1
     page_query_param='page'
+    page_size_query_param = 'page_size'
