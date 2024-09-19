e4b538c6792cfab3036cd26a45e04071ede569b7
Tuan Le <leanhtuan030523@gmail.com>
2024-09-19 20:39:11 +0700

    Add GetVendorInfo view

--- ecommerce/vendors/urls.py
+++ ecommerce/vendors/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CreateVendor
+from .views import CreateVendor, GetVendorInfo
 
 urlpatterns = [
-    path("new/", CreateVendor.as_view(), name="vendor_register")
+    path("new/", CreateVendor.as_view(), name="vendor_register"),
+    path("<str:pk>", GetVendorInfo.as_view(), name="vendor_info"),
 ]
--- ecommerce/vendors/views.py
+++ ecommerce/vendors/views.py
@@ -1,10 +1,26 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveAPIView
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+
 from .models import Vendor
-from .serializers import VendorSerializer
+from .serializers import VendorSerializer, VendorInfoSerializer
+
+from users.models import CustomUser
+
 # Create your views here.
 
 class CreateVendor(CreateAPIView):
     permission_classes = (AllowAny, )
     queryset = Vendor.objects.all()
     serializer_class = VendorSerializer
+    
+class GetVendorInfo(RetrieveAPIView):
+    permission_classes = (AllowAny, )
+    queryset = Vendor.objects.all()
+    serializer_class = VendorInfoSerializer
+    
+    def get_object(self):
+        user = get_object_or_404(CustomUser, id=self.kwargs["pk"])
+        vendor = get_object_or_404(Vendor, user=user)
+        return vendor
