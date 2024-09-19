43f31e6aa80455b54750dba51ec728894c177757
Tuan Le <leanhtuan030523@gmail.com>
2024-09-19 20:41:21 +0700

    Create ProductList to fetch vendor.products, fetch Vendor info

--- frontend/src/routes/vendor.$vendorID.tsx
+++ frontend/src/routes/vendor.$vendorID.tsx
@@ -1,7 +1,20 @@
+import ProductCard from '@/components/productCard';
+import { Product } from '@/types/product';
+import { useQuery } from '@tanstack/react-query';
 import { createFileRoute } from '@tanstack/react-router'
+
+const fetchVendorInfo = async (vendorID: string) => {
+  const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/vendor/${vendorID}`);
+  if (res.status !== 200) {
+    throw new Error('An error has occurred. Please try again later.')
+  }
+  
+  return await res.json()
+}
 
 export const Route = createFileRoute('/vendor/$vendorID')({
   component: VendorPage,
+  loader: ({params}) => fetchVendorInfo(params.vendorID),
 });
 
 function VendorPage() {
@@ -10,6 +23,42 @@ function VendorPage() {
   return (
     <div>
       <p>{vendorID}</p>
+      <ProductList vendorID={vendorID} />
+    </div>
+  )
+}
+
+interface ProductListProps{
+  vendorID: string;
+}
+
+function ProductList({vendorID}:ProductListProps) {
+    
+  const fetchProducts = async (): Promise<Product[]> => {
+      const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/products/vendor/${vendorID}`)
+      const data = await res.json();
+      if (res.status !== 200) throw data;
+
+      return data
+  }
+
+  const { isPending, isError, data: products } = useQuery({
+      queryKey: ['vendor_products'],
+      queryFn: fetchProducts,
+  });
+
+  if (isPending) return <div>Loading...</div>
+  if (isError) return <div>Error</div>
+  
+  console.log(products)
+
+  return (
+    <div>
+      <p>Product List</p>
+      {products.map((pd) => (
+        <ProductCard product={pd}/>
+        
+      ))}
     </div>
   )
 }
