398d289dc2d9bada7b1ac4feb108d007d691bd46
Tuan Le <leanhtuan030523@gmail.com>
2024-09-24 20:32:14 +0700

    Add vendor avatar, vendor cover photo

--- frontend/src/routes/vendor.$vendorID.tsx
+++ frontend/src/routes/vendor.$vendorID.tsx
@@ -1,9 +1,14 @@
 import ProductCard from '@/components/productCard';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Product } from '@/types/product';
+import { Vendor } from '@/types/user';
 import { useQuery } from '@tanstack/react-query';
 import { createFileRoute } from '@tanstack/react-router'
+import { UserRound } from 'lucide-react';
 
-const fetchVendorInfo = async (vendorID: string) => {
+import Cover from '../assets/images/background.jpg';
+
+const fetchVendorInfo = async (vendorID: string): Promise<Vendor> => {
   const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/vendor/${vendorID}`);
   if (res.status !== 200) {
     throw new Error('An error has occurred. Please try again later.')
@@ -19,10 +24,19 @@ export const Route = createFileRoute('/vendor/$vendorID')({
 
 function VendorPage() {
   const { vendorID } = Route.useParams();
+  const vendor = Route.useLoaderData();
 
   return (
     <div>
-      <p>{vendorID}</p>
+      <img src={vendor.cover_photo ? vendor.cover_photo : Cover} alt='' className='w-1/2 mx-auto'/>
+      <div className='bg-red-600 flex justify-center'>
+        <Avatar>
+          <AvatarImage src={vendor.avatar} />
+          <AvatarFallback>
+            <UserRound />
+          </AvatarFallback>
+        </Avatar>
+      </div>
       <ProductList vendorID={vendorID} />
     </div>
   )
@@ -50,8 +64,6 @@ function ProductList({vendorID}:ProductListProps) {
   if (isPending) return <div>Loading...</div>
   if (isError) return <div>Error</div>
   
-  console.log(products)
-
   return (
     <div>
       <p>Product List</p>
