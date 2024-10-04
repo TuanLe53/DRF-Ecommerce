2c1e6c4d11723f503f6c66d798163cb0f872430f
Tuan Le <leanhtuan030523@gmail.com>
2024-10-04 20:23:50 +0700

    Create CategoryDropDown

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,7 +1,8 @@
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 import { Toaster } from '@/components/ui/toaster';
 import { ContextProps, useAuth } from '@/contexts/authContext';
 import { QueryClient } from '@tanstack/react-query';
-import { createRootRouteWithContext, Link, Outlet, redirect } from '@tanstack/react-router'
+import { createRootRouteWithContext, Link, Outlet, redirect, useNavigate } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
 export interface RouterContext {
@@ -28,6 +29,7 @@ function RootComponent() {
       <div className='h-12 p-1 flex bg-red-200 items-center justify-between'>
         <div className='flex gap-2'>
           <Link to='/' className='text-lg [&.active]:font-bold'>Home</Link>
+          <CategoryDropDown />
         </div>
         {authState.isAuth ?
           <div className='flex'>
@@ -47,5 +49,29 @@ function RootComponent() {
       <Outlet />
       <TanStackRouterDevtools />
     </>
+  )
+}
+
+function CategoryDropDown() {
+  const navigate = useNavigate();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className='hover:underline hover:cursor-pointer  '>
+        <div>
+          <p className='text-lg'>Category<span className='text-sm'>▼</span></p>
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='w-56'>
+        <DropdownMenuGroup>
+          <DropdownMenuItem onClick={() => navigate({to:'/$category', params:{category: 'shirts'}})}>
+            Shirts
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => navigate({to:'/$category', params:{category: 'sneaker'}})}>
+            Sneaker
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
